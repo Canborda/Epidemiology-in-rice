903c38d9d498b4b325542990591992c8e1a31c7f
Camilo Borda <caabordagi@unal.edu.co>
2022-05-16 18:35:49 -0500

    Added login view

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public loginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  public message: string = "This is just a test";
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  login() {}
 
 }
