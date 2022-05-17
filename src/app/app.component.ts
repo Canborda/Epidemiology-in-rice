2c16eb95c1a8dc67f3468dc3772ee3ef436cb0fe
Camilo Borda <caabordagi@unal.edu.co>
2022-05-16 20:18:35 -0500

    Added login flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'Epidemiology-in-rice';
+  title = 'Epidemiology in rice';
+
+  constructor() {}
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,25 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AppComponent } from 'src/app/app.component';
+import { LoginService } from 'src/app/services/login.service';
+import { ROUTES } from 'src/utils/constants';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
   public loginForm = new FormGroup({
     email: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
   });
 
-  public message: string = "This is just a test";
+  constructor(
+    private _loginService: LoginService,
+    private _router: Router,
+    private _appComponent: AppComponent
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  login() {
+    if (!this.loginForm.value.email || !this.loginForm.value.password) {
+      alert('Datos incompletos');
+    } else {
+      this._loginService.login(this.loginForm.value).subscribe({
+        next: (res) => {
+          localStorage.setItem('access_token', res.access_token);
+          alert('Autenticación exitosa. Bienvenido!');
+          this._router.navigate([ROUTES.dashboard.home]);
+        },
+        error: (err) => {
+          alert('Error en autenticación');
+        },
+      });
+    }
   }
-
-  login() {}
-
 }
