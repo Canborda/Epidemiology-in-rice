bf823665922ba8ca9f3e9a3498832a293ba841e6
Camilo Borda <caabordagi@unal.edu.co>
2022-08-16 17:28:56 -0500

    Added crop model & service

--- src/app/dashboard/components/map-add/map-add.component.ts
+++ src/app/dashboard/components/map-add/map-add.component.ts
@@ -3,6 +3,9 @@ import { UntypedFormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 import { MapComponent } from '../map/map.component';
+import { CropI } from 'src/app/models/crop.model';
+
+import { CropsService } from 'src/app/services/crops.service';
 
 @Component({
   selector: 'app-map-add',
@@ -10,12 +13,20 @@ import { MapComponent } from '../map/map.component';
   styleUrls: ['./map-add.component.css'],
 })
 export class MapAddComponent implements OnInit {
+  crops!: CropI[];
   // Form variables
   name = new UntypedFormControl('', [Validators.required]);
 
-  constructor(public dialogRef: MatDialogRef<MapComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<MapComponent>,
+    private cropsService: CropsService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cropsService.getCrops().subscribe((result) => {
+      this.crops = result.data as CropI[];
+    });
+  }
 
   onSave() {
     if (!this.getNameErrorMessage()) {
